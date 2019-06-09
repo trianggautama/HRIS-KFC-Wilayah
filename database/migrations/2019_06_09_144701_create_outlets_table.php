84b2710b3388ded11a99f125d761c1cc9ec8b42b
unknown <tomycules@gmail.com>
2019-06-09 22:55:08 +0800

    add migration outlet table

---
+++ database/migrations/2019_06_09_144701_create_outlets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOutletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outlets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_kecamatan');
+            $table->string('telepon')->length(13);
+            $table->string('foto')->length(191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outlets');
+    }
+}
