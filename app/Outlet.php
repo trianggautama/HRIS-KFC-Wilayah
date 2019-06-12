c04fc04631d16197fb189f22fe6bc672dcc3b60c
unknown <tomycules@gmail.com>
2019-06-12 13:53:27 +0800

    add relation to user

--- app/Outlet.php
+++ app/Outlet.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Outlet extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User', 'id_user');
+      }
+      
 }
