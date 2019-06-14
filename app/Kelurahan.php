a73f0eac61202d990ec7f6a5d0eec5f1afb6f94f
unknown <tomycules@gmail.com>
2019-06-14 10:11:23 +0800

    add model kelurahan

---
+++ app/Kelurahan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelurahan extends Model
+{
+    protected $table ='kelurahans';
+    protected $fillable = [
+        'kode_kelurahan','kelurahan','kecamatan_id',
+    ];
+    
+    public function kecamatan(){
+        return $this->belongsTo('App\Kecamatan');
+    }
+}
