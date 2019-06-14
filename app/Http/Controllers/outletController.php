eee9375d862374468d14b1eaac9cf9431a001afa
unknown <tomycules@gmail.com>
2019-06-15 04:04:07 +0800

    add function profil edit

--- app/Http/Controllers/outletController.php
+++ app/Http/Controllers/outletController.php
@@ -2,20 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Outlet;
+use App\Karyawan;
+use Hash;
+use IDCrypt;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class outletController extends Controller
 {
 
     public function index(){
+        $id = auth::id();
+        // dd($id);
+        $Outlet  = Outlet::where('user_id', $id)->get();
 
-        return view('outlet.index');
+        return view('outlet.index',compact('Outlet','id'));
     }
 
-    public function edit_profil(){
+    public function profil_edit($id){
+        $id = IDCrypt::Decrypt($id);
+        $Outlet = Outlet::findOrFail($id);
+        dd($Outlet);
+        return view('outlet.profil_edit',compact('Outlet'));
+    }//fungsi outlet edit
+    // public function edit_profil(){
 
-        return view('outlet.edit_profil');
-    }
+    //     return view('outlet.edit_profil');
+    // }
 
     public function karyawan_data(){
 
