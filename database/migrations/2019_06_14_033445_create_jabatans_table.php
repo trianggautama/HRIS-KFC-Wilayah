980c704de53a74dfa5555c22fc1aadc7edac9a7f
unknown <tomycules@gmail.com>
2019-06-14 11:43:52 +0800

    add migration jabatan table

---
+++ database/migrations/2019_06_14_033445_create_jabatans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJabatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jabatans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode_jabatan')->length(20);
+            $table->string('jabatan')->length(191);
+            $table->string('tugas')->length(191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jabatans');
+    }
+}
