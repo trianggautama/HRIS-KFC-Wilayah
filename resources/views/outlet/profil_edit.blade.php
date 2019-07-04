3cc725c2f0eb71edd04adb3b6a50119c1176fb42
muhammadasadbastomi <tomycules@gmail.com>
2019-07-04 08:49:46 +0800

    add form outlet edit

--- resources/views/outlet/profil_edit.blade.php
+++ resources/views/outlet/profil_edit.blade.php
@@ -15,49 +15,42 @@
                                 class="icofont icofont-code-alt"></i></a>
                     </div>
                 </div>
-
+                <form  method="post" action="{{ route('admin_outlet_update', ['id' => IDCrypt::Encrypt( $outlet_datas->id)])}}" enctype="multipart/form-data">
+                        {{method_field('PUT') }}
+                            {{ csrf_field() }}
                 <div class="card-block">
-                    <div class="form-group row">
-                        <div class="col-md-2"><label for="InputNormal" class="form-control-label">Nama Outlet</label>
-                        </div>
-                        <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"
-                                placeholder="Nama"></div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="col-md-2"><label for="exampleSelect1" class="form-control-label">Kecamatan</label>
-                        </div>
-                        <div class="col-md-10">
-                            <select class="form-control" id="exampleSelect1">
-                                <option>Banjarbaru Utara</option>
-                                <option>Banjarbaru Selatan</option>
-                                <option>Sungai Andai</option>
-                                <option>Banjarmasin</option>
-                            </select>
-                        </div>
+                <div class="form-group row">
+                        <div class="col-md-2"><label for="InputNormal" class="form-control-label">Alamat</label></div>
+                        <div class="col-md-10"><textarea name="alamat" id="" class="form-control">{{ $outlet_datas->alamat }}</textarea></div>
+                </div>
+                <div class="form-group row">
+                        <div class="col-md-2"><label for="exampleSelect1" class="form-control-label">Kelurahan</label></div>
+                        <div class="col-md-10"> <select class="form-control" id="exampleSelect1"
+                                name="kelurahan_id">
+                                @foreach ($kelurahan as $j)
+                                <option value="{{ $j->id}}"
+                                    {{ $outlet_datas->kelurahan_id == $j->id ? 'selected' : ''}}>{{$j->kelurahan}}
+                                </option>
+                                @endforeach
+                            </select></div>
                     </div>
 
-                    <div class="form-group row">
-                        <div class="col-md-2"><label for="InputNormal" class="form-control-label">Alamat</label></div>
-                        <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"
-                                placeholder="Alamat"></div>
-                    </div>
-                    <div class="form-group row">
+                <div class="form-group row">
                         <div class="col-md-2"><label for="InputNormal" class="form-control-label">No Tlp</label></div>
-                        <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"
-                                placeholder="No.Tlp"></div>
-                    </div>
+                        <div class="col-md-10"><input name="telepon" type="text" class="form-control" id="InputNormal" value="{{ $outlet_datas->telepon }}" placeholder="No.Tlp"></div>
+                </div>
                     <div class="form-group row">
-                        <div class="col-md-2"><label for="InputNormal" class="form-control-label">Username</label></div>
-                        <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"
-                                placeholder="Isi Jika ingin mengganti username"></div>
+                            <label for="file" class="col-md-2 col-form-label form-control-label">Gambar/Logo</label>
+                            <div class="col-md-9">
+                                <label for="file" class="custom-file">
+                                    <input type="file" name="foto" id="file" class="custom-file-input">
+                                    <span class="custom-file-control"></span>
+                                </label>
+                            </div>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-md-2"><label for="InputNormal" class="form-control-label">Password</label></div>
-                        <div class="col-md-10"><input type="text" class="form-control" id="InputNormal"
-                                placeholder="Isi Jika ingin mengganti Password"></div>
-                    </div>
+                            {{ csrf_field() }}
                     <div class="card-footer text-right">
-                        <a href="" class="btn btn-inverse-primary">Ubah Data</a>
+                            <button type="submit" class="btn btn-primary mr-2">Simpan</button>
                     </div>
                 </div>
             </div>
@@ -65,5 +58,5 @@
         <!--input sizes ends-->
 
     </div>
-</div>
+    </div>
 @endsection
