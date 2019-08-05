1788490a8357dceca5414ae30b004a8113981933
tri angga utama <trianggategarutama@gmail.com>
2019-08-06 00:38:50 +0800

    ubah tampilan

--- resources/views/outlet/penilaian_karyawan_tambah.blade.php
+++ resources/views/outlet/penilaian_karyawan_tambah.blade.php
@@ -20,7 +20,7 @@
                     <form method="post" action="">
                         <!--inputannya di loop aja sesuai data objek penilaian-->
                      <div class="form-group row">
-                            <div class="col-md-2"><label for="InputNormal" class="form-control-label">Outlet</label></div>
+                            <div class="col-md-2"><label for="InputNormal" class="form-control-label">Karyawan</label></div>
                             <div class="col-md-10">
                             <select class="form-control" name="karyawan_id">
                             <option value="" >-Pilih Karyawan-</option>
